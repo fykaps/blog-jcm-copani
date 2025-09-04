52ad8315c7c6f38c10830e977bc0275d949f3310
BinationalLibrary <libreriabinacional@gmail.com>
2025-09-04 07:37:20 -0500

    0409250737

--- assets/js/components.js
+++ assets/js/components.js
@@ -411,7 +411,7 @@ function loadFooter() {
                             <svg viewBox="0 0 24 24" width="16" height="16" class="contact-icon">
                                 <path fill="currentColor" d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
                             </svg>
-                            Calle Principal 123, Ciudad
+                            A. El Estudiante S/N, Copani
                         </li>
                         <li class="contact-item">
                             <svg viewBox="0 0 24 24" width="16" height="16" class="contact-icon">
